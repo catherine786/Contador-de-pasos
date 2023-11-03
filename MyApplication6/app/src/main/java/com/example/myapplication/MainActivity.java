88971d28cb200f22d1192a6bfb8ed9b959309d25
catherine <83716701+catherine786@users.noreply.github.com>
2023-11-03 20:21:31 -0300

    Update MainActivity.java

--- MyApplication6/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication6/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,17 +15,15 @@ public class MainActivity extends AppCompatActivity {
     // Función que se ejecuta cuando se hace clic en un CardView
     public void onCardViewClick(View view) {
         Intent intent;
-        switch (view.getId()) {
-            case R.id.cardViewRutinaLibre:
-                // Aquí puedes abrir la actividad de "Rutina Libre"
-                intent = new Intent(this, RutinaLibreActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.cardViewNuevoReto:
-                // Aquí puedes abrir la actividad de "Nuevo Reto"
-                intent = new Intent(this, NuevoRetoActivity.class);
-                startActivity(intent);
-                break;
+
+        if (view.getId() == R.id.cardViewRutinaLibre) {
+            // Abre la actividad de "Rutina Libre"
+            intent = new Intent(this, RutinaLibreActivity.class);
+            startActivity(intent);
+        } else if (view.getId() == R.id.cardViewNuevoReto) {
+            // Abre la actividad de "Nuevo Reto"
+            intent = new Intent(this, NuevoRetoActivity.class);
+            startActivity(intent);
         }
     }
 }
